ef01b1e2f8252790232b60faa1d32b8063cd1141
Michael Lambert <michael@alphageek.com.au>
2019-12-27 11:10:52 +1100

    Improve javascript syntax

--- index.js
+++ index.js
@@ -2,40 +2,46 @@ const yaml = require('yaml-front-matter');
 const request = require('sync-request');
 const moment = require('moment');
 
-hexo.extend.filter.register(
-	'before_post_render',
-	function(data) {
-		let badgeUrl = yaml.loadFront(data.raw).badge;
-		if (badgeUrl !== undefined){
-			data.badge = getBadgeData('assertion', badgeUrl);
+const processDates = (data, badge) => {
+	if (typeof badge.issuedOn !== 'undefined') {
+		data.date = badge.issuedOn;
+		data.updated = badge.issuedOn;
+	}
 
-			if (typeof data.photos === 'undefined') data.photos = [];
+	return data;
+};
 
-			if (typeof data.badge.issuedOn !== 'undefined') {
-				data.date = data.badge.issuedOn;
-				data.updated = data.badge.issuedOn;
-			}
+const overridePostData = (data, badge) => {
+	data.title = badge.name;
+	data.photos.push(badge.image);
+	data.photo = badge.image;
+	data.description = badge.description;
 
-			if (typeof data.badge.badge !== 'undefined') {
-				data.title = data.badge.badge.name;
-				data.photos.push(data.badge.badge.image);
-				data.photo = data.badge.badge.image;
-				data.description = data.badge.badge.description;
-			} else {
-				data.title = data.badge.name;
-				data.photos.push(data.badge.image);
-				data.photo = data.badge.image;
-				data.description = data.badge.description;
-			}
+	return data;
+};
 
-			data.verify = verificationUrl(data.badge);
+const beforePostRender = (data) => {
+	let badgeUrl = yaml.loadFront(data.raw).badge;
+	if (badgeUrl !== undefined){
+		data.badge = getBadgeData('assertion', badgeUrl);
+
+		if (typeof data.photos === 'undefined') data.photos = [];
+
+		data = processDates(data, data.badge);
+
+		if (typeof data.badge.badge !== 'undefined') {
+			data = overridePostData(data, data.badge.badge);
+		} else {
+			data = overridePostData(data, data.badge);
 		}
 
-		return data;
+		data.verify = verificationUrl(data.badge);
 	}
-);
 
-let verificationUrl = function (assertion) {
+	return data;
+};
+
+const verificationUrl = (assertion) => {
 	switch (assertion.badge.issuer.url) {
 		case 'http://www.scrum.org':
 		case 'https://www.scrum.org':
@@ -48,7 +54,64 @@ let verificationUrl = function (assertion) {
 	}
 };
 
-let getBadgeData = function(type, content) {
+const processAssertion = (result) => {
+	if (typeof result.badge !== 'undefined') {
+		result.badge = getBadgeData('badgeClass', result.badge);
+	}
+	if (typeof result.image !== 'undefined') {
+		result.image = getBadgeData('image', result.image);
+	}
+	if (typeof result.evidence !== 'undefined') {
+		result.evidence = getBadgeData('image', result.evidence);
+	}
+	if (typeof result.issuedOn !== 'undefined') {
+		if (moment(result.issuedOn, moment.ISO_8601, true).isValid()) {
+			result.issuedOn = moment.utc(result.issuedOn, moment.ISO_8601);
+		} else {
+			result.issuedOn = moment.utc(result.issuedOn, 'X');
+		}
+	}
+
+	return result;
+};
+
+const processBadgeClass = (result) => {
+	if (typeof result.image !== 'undefined') {
+		result.image = getBadgeData('image', result.image);
+	}
+	if (typeof result.criteria !== 'undefined') {
+		result.criteria = getBadgeData('criteria', result.criteria);
+	}
+	if (typeof result.issuer !== 'undefined') {
+		result.issuer = getBadgeData('profile', result.issuer);
+	}
+
+	return result;
+};
+
+const processProfile = (result) => {
+	if (typeof result.image !== 'undefined') {
+		result.image = getBadgeData('image', result.image);
+	}
+	if (typeof result.publicKey !== 'undefined') {
+		result.publicKey = getBadgeData('cryptographicKey', result.publicKey);
+	}
+	if (typeof result.revocationList !== 'undefined') {
+		result.revocationList = getBadgeData('revocationList', result.revocationList);
+	}
+
+	return result;
+};
+
+const processObject = (result) => {
+	if (typeof result === 'object') {
+		result = result.id;
+	}
+
+	return result;
+};
+
+const getBadgeData = (type, content) => {
 	let resultBody = {};
 
 	// In some cases the requested field can contain either a document or a remote object
@@ -67,51 +130,19 @@ let getBadgeData = function(type, content) {
 
 	switch (type) {
 		case 'assertion': // Assertion
-			if (typeof resultBody.badge !== 'undefined') {
-				resultBody.badge = getBadgeData('badgeClass', resultBody.badge);
-			}
-			if (typeof resultBody.image !== 'undefined') {
-				resultBody.image = getBadgeData('image', resultBody.image);
-			}
-			if (typeof resultBody.evidence !== 'undefined') {
-				resultBody.evidence = getBadgeData('image', resultBody.evidence);
-			}
-			if (typeof resultBody.issuedOn !== 'undefined') {
-				if (moment(resultBody.issuedOn, moment.ISO_8601, true).isValid()) {
-					resultBody.issuedOn = moment.utc(resultBody.issuedOn, moment.ISO_8601);
-				} else {
-					resultBody.issuedOn = moment.utc(resultBody.issuedOn, 'X');
-				}
-			}
+			resultBody = processAssertion(resultBody);
 			break;
 		case 'badgeClass': // BadgeClass
-			if (typeof resultBody.image !== 'undefined') {
-				resultBody.image = getBadgeData('image', resultBody.image);
-			}
-			if (typeof resultBody.criteria !== 'undefined') {
-				resultBody.criteria = getBadgeData('criteria', resultBody.criteria);
-			}
-			if (typeof resultBody.issuer !== 'undefined') {
-				resultBody.issuer = getBadgeData('profile', resultBody.issuer);
-			}
+			resultBody = processBadgeClass(resultBody);
 			break;
 		case 'profile': // Profile
-			if (typeof resultBody.image !== 'undefined') {
-				resultBody.image = getBadgeData('image', resultBody.image);
-			}
-			if (typeof resultBody.publicKey !== 'undefined') {
-				resultBody.publicKey = getBadgeData('cryptographicKey', resultBody.publicKey);
-			}
-			if (typeof resultBody.revocationList !== 'undefined') {
-				resultBody.revocationList = getBadgeData('revocationList', resultBody.revocationList);
-			}
+			resultBody = processProfile(resultBody);
 			break;
 		case 'criteria': // Criteria
 		case 'image': // Image
-			if (typeof resultBody === 'object') {
-				resultBody = resultBody.id;
-			}
+			resultBody = processObject(resultBody);
 			break;
+		// TODO: Add support for the following items
 		case 'evidence': // Evidence
 		case 'cryptographicKey': // CryptographicKey
 		case 'revocationList': // RevocationList
@@ -119,3 +150,8 @@ let getBadgeData = function(type, content) {
 
 	return resultBody;
 };
+
+hexo.extend.filter.register(
+	'before_post_render',
+	beforePostRender
+);
